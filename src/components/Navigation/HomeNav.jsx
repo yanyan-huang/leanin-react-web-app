a796dc7f6cbbd6823df17dd24d0b715b0f39f506
sirancao <cao.sir@northeastern.edu>
2023-10-30 15:38:40 -0700

    add nav bar, bootswatch theme

--- src/components/Navigation/HomeNav.jsx
+++ src/components/Navigation/HomeNav.jsx
@@ -1,8 +1,95 @@
+import { useSelector } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import * as ROUTES from '../../constants/routes';
+
 function HomeNav() {
+    const { user } = useSelector((state) => state.userInfo);
+    const { firstName, role, isLogined } = user;
+    const isAdmin = role === 'admin';
+    const isUser = role === 'user';
+    const isCompany = role === 'company';
 
     return (
-        <><h1>Navbar</h1></>
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <Container fluid>
+                <Navbar.Brand>
+                    <img
+                        alt="logo"
+                        className="logo_size"
+                        src="/images/logo-transparent-png.png"
+                    />
+                </Navbar.Brand>
+                <Nav className="ml-auto">
+                    {isLogined ? (
+                        <LinkContainer to={isCompany ? ROUTES.COMPANY_PROFILE : ROUTES.HOME}>
+                            <Nav.Link>Hello, {firstName}</Nav.Link>
+                        </LinkContainer>
+                    ) : (
+                        <LinkContainer to={ROUTES.SIGN_IN}>
+                            <Nav.Link>Sign In</Nav.Link>
+                        </LinkContainer>
+                    )}
 
+                    {
+                        <LinkContainer to={ROUTES.HOME}>
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                    }
+
+
+                    {/* {isUser && (
+            <LinkContainer to={ROUTES.HOME}>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+          )}
+
+          {isAdmin && (
+            <LinkContainer to={ROUTES.HOME}>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+          )}
+
+          {isCompany && (
+            <LinkContainer to={ROUTES.HOME}>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+          )} */}
+
+
+                    {isCompany && (
+                        <LinkContainer to={ROUTES.COMPANY_PROFILE}>
+                            <Nav.Link>Company Profile</Nav.Link>
+                        </LinkContainer>
+                    )}
+
+                    {isUser && (
+                        <LinkContainer to={ROUTES.SEARCH}>
+                            <Nav.Link>Search</Nav.Link>
+                        </LinkContainer>
+                    )}
+
+                    {isUser && isLogined && (
+                        <LinkContainer to={ROUTES.PROFILE}>
+                            <Nav.Link>Profile</Nav.Link>
+                        </LinkContainer>
+                    )}
+
+                    {/* {isAdmin && (
+            <LinkContainer to={ROUTES.ADMIN}>
+              <Nav.Link>Users Management</Nav.Link>
+            </LinkContainer>
+          )} */}
+                    {isLogined && (
+                        <LinkContainer to={ROUTES.SIGN_OUT}>
+                            <Nav.Link>Sign Out</Nav.Link>
+                        </LinkContainer>
+                    )}
+                </Nav>
+            </Container>
+        </Navbar>
     );
 }
 
