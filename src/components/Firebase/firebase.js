4d41a7c1484d8374c8eb2cf190198767ebcb3b1d
ch722bruce <brucehuang722@gmail.com>
2023-12-14 15:48:00 -0800

    new

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,16 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyAC5W1amlvkDSaLjO0dEp5njhqGiiTjZG8",
-    authDomain: "leanin-db.firebaseapp.com",
-    projectId: "leanin-db",
-    storageBucket: "leanin-db.appspot.com",
-    messagingSenderId: "974533681074",
-    appId: "1:974533681074:web:ad5ff888e037e37ad90eab",
-    measurementId: "G-SQZP9ZPP2Y"
+  apiKey: "AIzaSyAC5W1amlvkDSaLjO0dEp5njhqGiiTjZG8",
+  authDomain: "leanin-db.firebaseapp.com",
+  projectId: "leanin-db",
+  storageBucket: "leanin-db.appspot.com",
+  messagingSenderId: "974533681074",
+  appId: "1:974533681074:web:ad5ff888e037e37ad90eab",
+  measurementId: "G-SQZP9ZPP2Y",
 };
 
 const app = initializeApp(config);
