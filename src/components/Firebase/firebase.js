b23732b8eb7aadf15049cdc465f81f6cecd1f3ef
Siran Cao <105324762+SiranCao579@users.noreply.github.com>
2023-12-17 18:09:21 -0800

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,15 +3,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyAC5W1amlvkDSaLjO0dEp5njhqGiiTjZG8",
-  authDomain: "leanin-db.firebaseapp.com",
-  projectId: "leanin-db",
-  storageBucket: "leanin-db.appspot.com",
-  messagingSenderId: "974533681074",
-  appId: "1:974533681074:web:ad5ff888e037e37ad90eab",
-  measurementId: "G-SQZP9ZPP2Y",
-};
+// remove config prevent exposure
+const config = {};
 
 const app = initializeApp(config);
 const auth = getAuth(app);
